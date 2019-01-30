f413e7bec4e789b0e138e9c777836770805746e1
Giorgos <gggiorgos1994@gmail.com>
2019-01-30 02:04:04 +0200

    navbar css & introduction text

--- navButtons.js
+++ navButtons.js
@@ -1,4 +1,7 @@
-function showContent( ShowElem , hideElem ){
+function showContent( ShowElem , hideElem , activeButton , inactiveButton){
 	hideElem.style.display="none";
 	ShowElem.style.display="inline-table";
+	
+	activeButton.style.backgroundColor="#111";
+	inactiveButton.style.backgroundColor="#484f4f";
 }
